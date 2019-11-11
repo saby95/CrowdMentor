b89b07f76fa1be82bc709c7939c82a0bae23e022
Naghma21 <30525641+Naghma21@users.noreply.github.com>
2019-11-10 20:04:36 -0600

    thread room link between single mentor and worker

--- chat/views.py
+++ chat/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Room
 from tasks.models import ResearchTasks,TaskUserJunction
 from django.contrib.auth.models import User
-from profile import Profile
+from users.models import Profile
 
 
 @login_required
@@ -13,22 +13,42 @@ def index(request):
     login and admin parts.
     """
     # Get a list of rooms, ordered alphabetically
+    import json
     rooms = Room.objects.order_by("title")
-    roomslist = [];
+    roomslist = []
+    chat_participants = []
     user = User.objects.get(username=request.user.username)
     tuj_list =TaskUserJunction.objects.filter(worker_id = user)
-    """roomslist = [];
-    for room in rooms:
-        roomslist.append(room.title)
-    for tuj in tuj_list:
-        if(str(tuj) not in roomslist):
-            room = Room(title=tuj, staff_only=False)
-            room.save()
-            roomslist = (Room.objects.values_list('title', 'id'));
-            roomId = roomslist[len(roomslist)-1][1];
-            TaskUserJunction.objects.filter(task_id_id=tuj.task_id.id).update(room_id=roomId)"""
+    role = request.user.profile.role
+    participants = Profile.objects.filter(user_id=request.user.profile.user_id)
+    mentee_task_list = {};
+    mentor_boolean  = 'false';
+    for p in participants:
+        if(p.is_Mentor):
+            mentor_boolean = 'true';
+        else:
+            mentor_boolean = 'false';
+        if(p.is_Mentor == True):
+            mentee_id = p.get_mentees();
+            #print(mentee_id);
+            for m in mentee_id:
+                user = User.objects.get(id=m);
+                tuj_list = TaskUserJunction.objects.filter(worker_id = user)
+                chat_participants.append(user.username);
+                
+                task_list = [];
+                for tuj in tuj_list:
+                    task_list.append((str(tuj),tuj.room_id));
+                mentee_task_list[user.username] = task_list;
+        else:
+            mentor_id = p.get_mentors()[0];
+            user = User.objects.get(id=mentor_id);
+            chat_participants.append(user.username);
             
     # Render that in the index template
     return render(request, "messages.html", {
         "tuj_list": tuj_list,
+        "chat_participants" : chat_participants,
+        "isMentor" : mentor_boolean,
+        "mentee_task_list" : mentee_task_list
     })
