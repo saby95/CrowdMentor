2c28347bb82c0df76e06342d6b552ff4686b6dd4
ManasaDG <dgmanasa1@gmail.com>
2019-12-10 19:19:40 -0600

    test cases bugs fixed

--- chat/urls.py
+++ chat/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns =[
     path('',views.index),
     path('save_message/',views.save_message,name='save_message'),
-    path('message_thread/',views.message_thread,name='message_thread'),
-    path('update_message/',views.update_message,name='update_message')
+    path('message_thread/',views.message_thread,name='message_thread')
 ]
--- features/change_roles 2.feature
+++
@@ -1,9 +0,0 @@
-Feature: Change role form
-
-  Scenario: Access the change role form
-
-    Given I am an existing user with admin access
-    And there is one user with worker access
-    And I am logged in as the user with admin access
-    When I change any user role
-    Then I am redirected to the home page
